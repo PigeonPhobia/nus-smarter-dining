5305003ed1a3eb42e92b5a87fcdf0324cb49e5c6
pigeon-phobia <jbq3712@gmail.com>
2023-03-15 17:20:40 +0800

    Implement canteen listing

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
-  <v-app
-    :theme="theme"
-    class="rounded-lg border"
-    style="max-height: 200px;"
-  >
-    <v-app-bar>
+  <v-app>
+    <v-app-bar color="orange">
+      <v-btn>Back</v-btn>
       <v-spacer></v-spacer>
-
-      <v-btn
-        :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
-        @click="onClick"
-      >Toggle Theme</v-btn>
+      <v-btn @click="onClickCart">Cart</v-btn>
     </v-app-bar>
 
-    <v-main>
-      <v-container class="text-center">Content area</v-container>
-    </v-main>
+    <v-container>
+      <v-row>
+        <v-spacer></v-spacer>
+        <v-col v-for="canteen in canteens" :key="canteen" cols="6">
+          <v-card @click="onClickCard(canteen)" style="height: 250px;">
+            <v-card-text>{{ canteen }}</v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app>
 </template>
 
 <script setup>
-  import { ref } from 'vue'
-  const theme = ref('light')
-  function onClick () {
-    theme.value = theme.value === 'light' ? 'dark' : 'light'
+  let error = 0
+  const canteens = [
+    "The Terrace",
+    "The Summit", 
+    "PGPR", 
+    "Frontier (Non Aircon)",
+    "Frontier (Aircon)",
+    "Techno Edge",
+    "The Deck"
+  ]
+  function onClickCart () {
+    console.log("Click Cart!!!")
+  }
+  function onClickCard (canteen) {
+    if (canteen !== "The Terrace") { error++ }
+    console.log("Click Card", canteen, "! Current error: ", error)
   }
 </script>
